384407eb72f50521267d9d36a3e8b55249f635d6
Brian Wilson <bountonw@gmail.com>
2017-03-15 18:37:07 +0000

    Validate user data

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ApplicationRecord
+  validates :tagline, presence: true, length: { maximum: 50, minimum: 5 }
+  validates :saying, presence: true, length: { maximum: 280, minimum: 3 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
